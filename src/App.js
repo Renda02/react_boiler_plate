91192cf5bff2e7b1d34ada03fee840d2fa95f317
Rendani <rendaniluvhengo89@gmail.com>
2021-07-03 15:46:14 +0200

    adding Pagination

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Switch } from "react-router-dom";
 import "./App.css";
-import PostList from "./PostList";
+import PostList from "./components/PostList";
 
 function App() {
   return (
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -3,7 +3,9 @@ import Comments from './Comments';
 
 const PostList = () => {
     const [postList, setPostList] = useState([]);
-    const [currentPage, setCurrentPage] = useState(0)
+    const [currentPage, setCurrentPage] = useState(0);
+    const [totalNrPages, setTotalNrPages] = useState(1); 
+    const [postPerPage, setPostPerPage] = useState(10);
 
 useEffect(() => {
      fetch(`https://jsonplaceholder.typicode.com/posts?_page=${currentPage}&_limit=10`)
@@ -11,22 +13,31 @@ useEffect(() => {
         .then((data) =>{
             console.log(data)
             setPostList(data)
+
+            
           })
 }, [currentPage])
 
-    
+const pageNumberList = [];
+for (let i = 1; i < totalNrPages; i++) {
+  pageNumberList.push(i);
+}
 
     return (
-        <div>
+        <div >
             {postList.map((post)=>{
                 return<div>
                     <p>Title:{post.title}</p>
                     <Comments  postId={post.id} />
                 </div>
-              
-            })
-
-            }
+            }) }
+            <div>
+		  {
+			  pageNumberList.map((pageNumber)=>{
+				  return  <button onClick={(()=>{setCurrentPage(pageNumber)})}>{pageNumber}</button>
+			  })
+		  }
+	  </div>
         </div>
     )
 }
